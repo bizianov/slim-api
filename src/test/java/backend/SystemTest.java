68a8637a0561f12e97a82ceb592a752e1cf3ff45
Bizianov, Viacheslav <>
2017-11-13 11:21:16 +0200

    system + integration tests

---
+++ src/test/java/backend/SystemTest.java
@@ -0,0 +1,42 @@
+package backend;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SystemTest {
+
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void init() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
+
+    @Test
+    public void allCustomers() throws Exception {
+        mockMvc.perform(get("/customers"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(allOf(
+                        containsString("Bizianov"),
+                        containsString("Viacheslav"))));
+    }
+}
